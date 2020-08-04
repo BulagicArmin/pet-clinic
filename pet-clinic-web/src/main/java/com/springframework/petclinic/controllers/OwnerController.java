146d756fe427532057319a54c738640abe0c2aff
Armin Bulagic <armin.bulagic2018@size.ba>
2020-08-04 19:23:42 +0200

    Created Owner index page and controller. Closes #11.

---
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+    @RequestMapping({"","/","index","index.html"})
+    public String ownerList(){
+        return "owners/index";
+    }
+}
